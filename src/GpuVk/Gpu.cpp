d17473f006a95e22de156c374773352e89df8502
Zorbn <public.zorb@gmail.com>
2023-06-16 22:06:45 -0700

    Update application info

--- src/GpuVk/Gpu.cpp
+++ src/GpuVk/Gpu.cpp
@@ -49,7 +49,7 @@ void Gpu::CreateInstance(SDL_Window* window)
 
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.pApplicationName = "GpuVk";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
